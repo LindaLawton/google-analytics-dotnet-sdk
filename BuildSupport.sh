71ef2a9a61610815ecb056ee94bd6a4a4efa9524
Linda Lawton <laurly71@gmail.com>
2018-06-08 17:21:09 +0200

    Create BuildSupport.sh

---
+++ BuildSupport.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+set -e
+
+# "nuget restore" fails if local package source directories don't exist.
+mkdir -p NuPkgs/Support
+
+# Final output directory of NuPkgs.
+NUPKG_DIR=`pwd`/NuPkgs/Support
+# Build configuration to build/pack.
+BUILD_CONFIGURATION=Release
+
+# Forces sourcelink to work during the build.
+export CI=true
+
+dotnet restore Src/Google.Analytics.SDK.sln
+dotnet build Src/Google.Analytics.SDK.sln --configuration $BUILD_CONFIGURATION --no-restore
+dotnet pack Src/Google.Analytics.SDK.sln --configuration $BUILD_CONFIGURATION --no-restore --no-build --output $NUPKG_DIR
