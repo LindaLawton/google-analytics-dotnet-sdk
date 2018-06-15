7c637f72c5d82085bed88e6a3f911d7a6ee6eabb
Linda Lawton <lindalawton@daimto.com>
2018-06-15 13:49:51 +0200

    item name required for item hittype

--- src/Google.Analytics.SDK.Core/Hits/ItemHit.cs
+++ src/Google.Analytics.SDK.Core/Hits/ItemHit.cs
@@ -4,7 +4,7 @@ namespace Google.Analytics.SDK.Core.Hits
 {
     public class ItemHit : ItemHitBase
     {
-        public ItemHit(string transactionId) : base(transactionId)
+        public ItemHit(string transactionId, string itemName) : base(transactionId, itemName)
         {
 
         }
