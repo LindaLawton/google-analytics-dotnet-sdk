8175c14716c1d74cdcc65ececa079ff20226cd6b
Linda Lawton <lindalawton@daimto.com>
2018-06-29 10:33:46 +0200

    Update deploy.sh
    debugging

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -3,5 +3,7 @@ NUPKG_DIR=$2
 
 nuget pack .nuspec -Verbosity detailed
 
-nuget push $NUPKG_DIR/*.nupkg -Verbosity detailed -ApiKey $ApiKey -Source https://api.nuget.org/v3/index.json
+ls -la $NUPKG_DIR/*.nupkg
+
+# nuget push $NUPKG_DIR/*.nupkg -Verbosity detailed -ApiKey $ApiKey -Source https://api.nuget.org/v3/index.json
 
