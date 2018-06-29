dd455adebd257353189bafc37f9932e6a1c3aa4d
Linda Lawton <lindalawton@daimto.com>
2018-06-29 13:20:23 +0200

    Update deploy.sh

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -4,7 +4,7 @@ ApiKey=$1
 echo "Deploy start."
 echo $ApiKey
 
-nuget pack .nuspec -Verbosity detailed
+#nuget pack .nuspec -Verbosity detailed
 
 #nuget push $NUPKG_DIR/*.nupkg -Verbosity detailed -ApiKey $ApiKey -Source https://api.nuget.org/v3/index.json
 
