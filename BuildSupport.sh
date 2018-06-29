2ad080c6b1e251575c9d5e61345410819ce9bfd5
Linda Lawton <lindalawton@daimto.com>
2018-06-29 14:29:46 +0200

    Update BuildSupport.sh

--- BuildSupport.sh
+++ BuildSupport.sh
@@ -1,28 +1,29 @@
 #!/bin/bash
 
+# -e  Exit immediately if a command exits with a non-zero status.
 set -e
 
-echo "Building support"
+echo "Building project"
 
-# "nuget restore" fails if local package source directories don't exist.
-mkdir -p NuPkgs/Support
+# Directory to output Nuget Package to.
+mkdir -p NuPkg
 
 # Final output directory of NuPkgs.
-NUPKG_DIR="$(pwd)/NuPkgs/Support"
+NUPKG_DIR="$(pwd)/NuPkg"
 # Final output directory of NuPkgs.
 NUSPEC_PATH="$(pwd)/deploy/.nuspec"
 # Build configuration to build/pack.
 BUILD_CONFIGURATION=Release
 
-echo $NUPKG_DIR
-ls -la $NUPKG_DIR
-echo $NUSPEC_PATH
+echo "Loading .nuspec from: $NUSPEC_PATH"
+echo "Output NuGet package to: $NUPKG_DIR"
 
 # Forces sourcelink to work during the build.
 export CI=true
 
-#dotnet build -c $BUILD_CONFIGURATION src/Google.Analytics.SDK.Core
-#dotnet build -c $BUILD_CONFIGURATION tests/Google.Analytics.SDK.Tests
-#dotnet test -c $BUILD_CONFIGURATION tests/Google.Analytics.SDK.Tests 
-#dotnet pack src/Google.Analytics.SDK.Core --configuration $BUILD_CONFIGURATION --no-restore --no-build --output $NUPKG_DIR /p:NuspecFile=deploy/.nuspec
+dotnet build -c $BUILD_CONFIGURATION src/Google.Analytics.SDK.Core
+dotnet build -c $BUILD_CONFIGURATION tests/Google.Analytics.SDK.Tests
+dotnet test -c $BUILD_CONFIGURATION tests/Google.Analytics.SDK.Tests 
+dotnet pack src/Google.Analytics.SDK.Core --configuration $BUILD_CONFIGURATION --no-restore --no-build --output $NUPKG_DIR /p:NuspecFile=$NUSPEC_PATH
 
+ls -la $NUPKG_DIR
