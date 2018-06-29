07980bf132f62509edd0e79f25cec05e9ff6e6cd
Linda Lawton <lindalawton@daimto.com>
2018-06-29 14:53:03 +0200

    Update BuildSupport.sh

--- BuildSupport.sh
+++ BuildSupport.sh
@@ -11,11 +11,11 @@ mkdir -p NuPkg
 # Final output directory of NuPkgs.
 NUPKG_DIR="$(pwd)/NuPkg"
 # Final output directory of NuPkgs.
-NUSPEC_PATH="$(pwd)/deploy/nuspec.xml"
+NUSPEC_PATH="$(pwd)/deploy/.nuspec"
 # Build configuration to build/pack.
 BUILD_CONFIGURATION=Release
 
-echo "Loading .nuspec from: $NUSPEC_PATH"
+echo "Loading nuspec from: $NUSPEC_PATH"
 echo "Output NuGet package to: $NUPKG_DIR"
 
 # Forces sourcelink to work during the build.
@@ -24,8 +24,8 @@ export CI=true
 dotnet build -c $BUILD_CONFIGURATION src/Google.Analytics.SDK.Core
 dotnet build -c $BUILD_CONFIGURATION tests/Google.Analytics.SDK.Tests
 dotnet test -c $BUILD_CONFIGURATION tests/Google.Analytics.SDK.Tests 
-dotnet pack src/Google.Analytics.SDK.Core --configuration $BUILD_CONFIGURATION --no-restore --no-build --output 
+dotnet pack src/Google.Analytics.SDK.Core --configuration $BUILD_CONFIGURATION --no-restore --no-build --output #$NUPKG_DIR
 
-#$NUPKG_DIR /p:NuspecFile=$NUSPEC_PATH
+ # /p:NuspecFile=$NUSPEC_PATH
 
 ls -la $NUPKG_DIR
